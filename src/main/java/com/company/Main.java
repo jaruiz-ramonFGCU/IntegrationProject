f9171bf77332c62332a9533a41567115ebd53001
Jose Ruiz-Ramon <jaruizramon7195@eagle.fgcu.edu>
2020-02-06 19:57:13 -0800

    Iteration I

---
+++ src/main/java/com/company/Main.java
@@ -0,0 +1,27 @@
+package com.company;
+
+import com.google.gson.Gson;
+import com.google.gson.stream.JsonReader;
+import com.opencsv.exceptions.CsvValidationException;
+
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException, CsvValidationException {
+        // Literally just calls our parser right now (....and is called for tests)
+        CsvParser csvP = new CsvParser("src/Data/bookstore_report2.csv");
+        csvP.printCsv();
+
+        // Load the json
+        /*
+        1. Create instance of GSON
+        2. Create a JsonReader object using FileReader
+        3. Array of class instances of AuthorParser, assign data from our JsonReader
+        4. foreach loop to check data
+         */
+
+
+    }
+}
